5d6644364847dfc301a7d504d770471e62afc030
Andrew Stam <andrew.cstam@gmail.com>
2019-06-08 16:14:19 -0400

    Added bootstrap, setup page in state

--- src/containers/MovieClub.js
+++ src/containers/MovieClub.js
@@ -1,9 +1,18 @@
 // Created by Andrew Stam
 import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 export default class MovieClub extends React.Component {
     constructor(props) {
         super(props);
+        const pathname = window.location.pathname;
+        const paths = pathname.split('/');
+        const pageName = paths[2];
+
+        // Pages: /home, /login, /search, /details/{did}, /profile/{uid}
+        this.state = {
+            page: pageName
+        };
     }
 
     render() {
