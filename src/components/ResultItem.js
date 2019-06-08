8476db68533d15cef40db5b0052285ce157ab7fc
Andrew Stam <andrew.cstam@gmail.com>
2019-06-08 17:20:03 -0400

    Added stateless ResultItem to render search results as a list

---
+++ src/components/ResultItem.js
@@ -0,0 +1,8 @@
+// Created by Andrew Stam
+import React from 'react';
+
+const ResultItem = ({title}) =>
+    <div className="form-control"> Result: <strong>{title}</strong>
+    </div>
+
+export default ResultItem;
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,19 +1,49 @@
 // Created by Andrew Stam
 import React from 'react';
 import './SearchComponent.css';
+import ResultItem from './ResultItem';
 
 // Handles search queries to the omdb API
 export default class SearchComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            query: '',
+            results: []
+        };
+    }
+
+    // Send the query to omdb API, load result into state
+    // Function based on Jose Annunziato's lecture slides
+    submitQuery = () => {
+        var url = 'http://www.omdbapi.com';
+        // My personal API key, do not duplicate or reuse without permission
+        url += '?apikey=abfe6d09';
+        url += '&s=' + this.state.query;
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {this.setState({results: json.Search})});
+    }
+
     render() {
         return (
             <div>
                 <h3>Search Page</h3>
                 <div className="input-group wbdv-search-bar">
-                    <input type="text" className="form-control"
-                           placeholder="Search for anything movie-related!"/>
+                    <input type="text" className="form-control" value={this.state.query}
+                           onChange={e => this.setState({query: e.target.value})}
+                           placeholder="Search for a movie"/>
                     <div className="input-group-append">
-                        <button className="btn btn-success">Search</button>
+                        <button className="btn btn-success" type="submit"
+                                onClick={() => this.submitQuery()}>Search</button>
                     </div>
+                </div>
+                <div>
+                    <h4>Results</h4>
+                    {this.state.results.map((r, key) =>
+                        <ResultItem key={key} title={r.Title}/>
+                    )}
                 </div>
             </div>
         );
