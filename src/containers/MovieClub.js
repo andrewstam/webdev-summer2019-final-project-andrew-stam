ac0cbb61ec73e9d8354ce891b82b5952e8727639
Andrew Stam <andrew.cstam@gmail.com>
2019-06-08 15:59:25 -0400

    Created top-level container called MovieClub

---
+++ src/containers/MovieClub.js
@@ -0,0 +1,14 @@
+// Created by Andrew Stam
+import React from 'react';
+
+export default class MovieClub extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>Movie Club</div>
+        );
+    }
+}
